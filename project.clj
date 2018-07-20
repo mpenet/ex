4b3de5d81ca983b4f5e79289fb02c045550d2d4c
Max Penet <m@qbits.cc>
2018-07-20 16:44:49 +0200

    release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.qbits/ex "0.1.0-SNAPSHOT"
+(defproject cc.qbits/ex "0.1.0"
   :description "Yet another exception catching library"
   :url "https://github.com/mpenet/ex"
   :license {:name "Eclipse Public License"
